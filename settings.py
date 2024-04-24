e2b033af2ce0b1ed42933d9d1da3434c00e968ee
BigNomNom <jakub2007@atlas.cz>
2024-04-24 09:57:09 +0200

    added a settings module that stores a settings class

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """a class that stores all settings of the game"""
+    def __init__(self):
+        """initialize the game settings"""
+        self.screen.width = 1200
+        self.screen.height = 800
+        self.bg_color = (120,230,50)
+        
+        
