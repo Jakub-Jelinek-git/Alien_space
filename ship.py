01aa54dea005e3c4d483d70d867263056355d6ad
BigNomNom <jakub2007@atlas.cz>
2024-04-25 16:46:50 +0200

    added additional requirement so the ship doesnt exit the screen

--- ship.py
+++ ship.py
@@ -31,9 +31,9 @@ class Ship():
     def update(self):
         """update sthe ship movement"""
         #updates the ships center value not rect
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.game_settings.ship_speed_factor
-        if self.moving_left:
+        if self.moving_left and self.rect.left > self.screen_rect.left:
             self.center -= self.game_settings.ship_speed_factor
         
         #updates the ships rect value
