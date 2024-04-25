0a90f976b2b3b6c2af8ba0a2868d9a28e434c66a
BigNomNom <jakub2007@atlas.cz>
2024-04-25 17:00:32 +0200

    made a 2 new methods witch clean up the code a bit

--- game_functions.py
+++ game_functions.py
@@ -1,21 +1,24 @@
 """all the functionality of the game"""
 import sys
 import pygame
-
+def check_key_down_events(event,ship):
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_RIGHT:
+            ship.moving_right = True
+        if event.key == pygame.K_LEFT:
+            ship.moving_left = True
+def check_key_up_events(event, ship):
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_RIGHT:
+            ship.moving_right = False
+        if event.key == pygame.K_LEFT:
+            ship.moving_left = False
 def check_events(ship):
     for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                        ship.moving_right = True
-                if event.key == pygame.K_LEFT:
-                        ship.moving_left = True
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                        ship.moving_right = False
-                if event.key == pygame.K_LEFT:
-                        ship.moving_left = False
+        if event.type == pygame.QUIT:
+            sys.exit()
+        check_key_down_events(event,ship)
+        check_key_up_events(event,ship)
 def screen_unpdate(screen, settings, ship):
      #chenges the creen color each loop
         screen.fill(settings.bg_color)
