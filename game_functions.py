de72dcbb53355a4c1e9993bac362ec3131fa06ce
BigNomNom <jakub2007@atlas.cz>
2024-04-30 19:07:37 +0200

    added functions to chenge direction

--- game_functions.py
+++ game_functions.py
@@ -8,6 +8,7 @@ def fire_bullet(event,game_settings,bullets,screen,ship):
     if event.key == 32 and game_settings.bullets_allowed >= len(bullets)+1:
         new_bullet = Bullet(game_settings,screen,ship)
         bullets.add(new_bullet)
+
 def check_key_down_events(event,ship,game_settings,screen,bullets):
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_q:
@@ -17,18 +18,21 @@ def check_key_down_events(event,ship,game_settings,screen,bullets):
         elif event.key == pygame.K_LEFT:
             ship.moving_left = True
         fire_bullet(event,game_settings,bullets,screen,ship)
+
 def check_key_up_events(event, ship):
     if event.type == pygame.KEYUP:
         if event.key == pygame.K_RIGHT:
             ship.moving_right = False
         if event.key == pygame.K_LEFT:
             ship.moving_left = False
+
 def check_events(ship,screen,game_settings,bullets):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         check_key_down_events(event,ship,game_settings,screen,bullets)
         check_key_up_events(event,ship)
+
 def update_bullets(bullets):
     bullets.update()
     #updates bullet pos bullets
@@ -36,8 +40,10 @@ def update_bullets(bullets):
         if bullet.rect.bottom <=0:
             bullets.remove(bullet)
 def draw_bullets(bullets):
+
     for bullet in bullets.sprites():
          bullet.draw_bullet()
+
 def screen_unpdate(screen, settings, ship, aliens, bullets):
     #chenges the creen color each loop
         screen.fill(settings.bg_color)
@@ -49,17 +55,20 @@ def screen_unpdate(screen, settings, ship, aliens, bullets):
             alien.blitme()
         #draws the most recent screen with changes
         pygame.display.flip()
+
 def get_number_aliens_x(game_settings, alien_width):
     """returs the number of aliens possible on the screen"""
     available_space = game_settings.screen_width - (alien_width * 2)
     number_aliens_x = int(available_space  / (alien_width * 2))
     return number_aliens_x
+
 def get_number_rows_y(game_settings,alien_height,ship_height):
     """returns the number of rows possible on thi spage"""
     space_available = (game_settings.screen_height - 
                         (alien_height * 3) - ship_height)
     number_of_rows = int(space_available / (alien_height * 2))
     return number_of_rows 
+
 def create_allien(game_settings,screen,aliens,alien_number, rows_number):
     alien = Alien(game_settings, screen)
     alien_width = alien.rect.width
@@ -67,6 +76,7 @@ def create_allien(game_settings,screen,aliens,alien_number, rows_number):
     alien.rect.y = alien.rect.height + 2 * alien.rect.height * rows_number 
     alien.rect.centerx = alien.x
     aliens.add(alien)
+
 def create_alien_fleet(screen, game_settings, aliens, ship):
     """creates a fleet of aliens based on screen width"""
     alien = Alien(game_settings, screen)
@@ -75,6 +85,23 @@ def create_alien_fleet(screen, game_settings, aliens, ship):
     for row in range(number_of_rows):
         for alien_number in range(number_aliens_x):
             create_allien(game_settings,screen,aliens,alien_number,row)
+
+def update_aliens(aliens,settings):
+    """Update the postions of all aliens in the fleet."""
+    fleet_check_edges(aliens,settings)
+    aliens.update()
+
+def fleet_check_edges(aliens,settings):
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(aliens,settings)
+            break 
+
+def change_fleet_direction(aliens, settings):
+    for alien in aliens.sprites():
+        alien.rect.y += settings.alien_drop_factor
+    settings.alien_direction *= -1
+
         
 
 
